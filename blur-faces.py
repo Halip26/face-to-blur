36183599935901ada601b84ad80f390211878048
Halip26 <halipuddin.angko@gmail.com>
2023-09-30 23:53:17 +0800

    add path to caffe model

---
+++ blur-faces.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+import sys
+
+# Jalur prototxt model Caffe
+prototxt_path = "weights/deploy.prototxt.txt"
+
+# Jalur model Caffe
+model_path = "weights/res10_300x300_ssd_iter_140000_fp16.caffemodel"
