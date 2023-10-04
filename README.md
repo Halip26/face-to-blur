aca62ef13f51b28b4e25ff937c78e259572a260c
Halip26 <halipuddin.angko@gmail.com>
2023-10-05 01:44:59 +0800

    add forever looping to blur faces

--- README.md
+++ README.md
@@ -14,14 +14,56 @@ To run this:
 - To blur faces using your live camera:
 
     ```bash
-    python blur_faces_live.py
+    python live-blur-faces.py
     ```
 
 - To blur faces of a video:
 
     ```bash
-    python blur_faces_video.py video.3gp
+    python video-blur-faces.py video.mp4
     ```
+
+## Face Detection and Blurring Application
+
+This code is a simple face detection and blurring application using OpenCV. It detects faces in real-time from the default camera feed and applies a Gaussian blur to them. Here are the steps involved:
+
+1. Import the necessary libraries: cv2, numpy, and time.
+
+2. Define the paths to the prototxt file and model file for the Caffe model.
+
+3. Load the Caffe model using `cv2.dnn.readNetFromCaffe()`.
+
+4. Connect to the default camera using `cv2.VideoCapture()`.
+
+5. Enter a forever loop to continuously detect faces and apply blurring.
+
+6. Read the live frames from the camera capture.
+
+7. Get the width and height of the image.
+
+8. Preprocess the image by resizing it to (300, 300) and subtracting the mean values.
+
+9. Set the preprocessed image as the input to the neural network model.
+
+10. Perform inference and get the output from the model.
+
+11. Loop through the detected faces and check their accuracy level.
+
+12. If the face accuracy is above 40%, blur the bounding box region.
+
+13. Apply Gaussian blur to the detected face region.
+
+14. Replace the original face region with the blurred face in the image.
+
+15. Display the final image with blurred faces.
+
+16. Break the loop if the user presses the 'q' key.
+
+17. Calculate the frames per second (FPS) of the processing loop.
+
+18. Close all OpenCV windows and release the camera capture.
+
+Please note that this code assumes the presence of the necessary model files and weights in the specified paths.
 
 ## Output faces become blurred
 
--- live-blur-faces.py
+++ live-blur-faces.py
@@ -10,3 +10,48 @@ model_path = "weights/res10_300x300_ssd_iter_140000_fp16.caffemodel"
 
 # memuat model Caffe
 model = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
+
+# untuk menyambungkan ke kamera default
+cap = cv2.VideoCapture(0)
+
+# forever looping untuk deteksi wajah & buat blur
+while True:
+    start = time.time()
+    live, image = cap.read()
+    # mendapatkan lebar dan tinggi gambar
+    h, w = image.shape[:2]
+    kernel_width = (w // 7) | 1
+    kernel_height = (h // 7) | 1
+    # praproses gambar: ubah ukuran dan lakukan pengurangan rata-rata
+    blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300), (104.0, 177.0, 123.0))
+    # mengatur gambar menjadi input jaringan saraf tiruan
+    model.setInput(blob)
+    # lakukan inferensi dan dapatkan hasilnya
+    output = np.squeeze(model.forward())
+
+    # looping untuk manggambar blur padaarea wajah
+    for i in range(0, output.shape[0]):
+        face_accuracy = output[i, 2]
+        # dapatkan tingkat akurasi wajah
+        # jika akurasi wajah lebih dari 40%, maka buramkan kotak pembatas (wajah)
+        if face_accuracy > 0.4:
+            # dapatkan koordinat kotak di sekitarnya dan tingkatkan ke gambar asli
+            box = output[i, 3:7] * np.array([w, h, w, h])
+            # ubah menjadi bilangan bulat
+            start_x, start_y, end_x, end_y = box.astype(np.int)
+            # dapatkan gambar wajah
+            face = image[start_y:end_y, start_x:end_x]
+            # terapkan gaussian blur ke wajah terdeteksi
+            face = cv2.GaussianBlur(face, (kernel_width, kernel_height), 0)
+            # taruh wajah yang telah diburamkan ke dalam gambar asli
+            image[start_y:end_y, start_x:end_x] = face
+
+    cv2.imshow("Faces blurred v1.0", image)
+    if cv2.waitKey(1) == ord("q"):
+        break
+    time_elapsed = time.time() - start
+    fps = 1 / time_elapsed
+    print("FPS:", fps)
+
+cv2.destroyAllWindows()
+cap.release()
